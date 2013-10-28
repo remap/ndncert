0db51f8d1320bc0689807555bc9ca4a2ec226d69
Alexander Afanasyev <alexander.afanasyev@ucla.edu>
2013-10-28 11:03:39 -0700

    Adding a little bit more advanced version of adding operators to the database

--- root-operator-tools/add-operator.py
+++ root-operator-tools/add-operator.py
@@ -1,19 +1,31 @@
 #!/usr/bin/env python
 
 import pymongo
+import json
+import argparse
+import sys
+
+parser = argparse.ArgumentParser(description='Add/Replace operator information')
+parser.add_argument('input', metavar='input', type=str,
+                    help='''File with operator information in JSON format (see example for details).  Use - to input from stdin''')
+args = parser.parse_args()
+
+if (not args.input):
+    parser.print_help ()
+    exit (1)
+
+if args.input == '-':
+    json_input = sys.stdin
+else:
+    json_input = open(args.input)
+
+operator = json.load(json_input)
 
 client = pymongo.MongoClient("localhost", 27017)
 db = client.ndncert
 
-
-operator = {
-    'name': 'Alexander Afanasyev',
-    'email': 'afanasev@cs.ucla.edu',
-    'key': '',
-    'last_timestamp': '',
-    'site_emails': ['ucla.edu', 'cs.ucla.edu'],
-    'site_name': 'University of California, Los Angeles',
-    'site_prefix': '/ndn/edu/ucla'
-    }
-
+db.operators.remove({'site_prefix': operator['site_prefix']})
 db.operators.insert(operator)
+
+print "[%s] site's operator [%s] has been added or updated" % (operator['site_prefix'], operator['name'])
+
